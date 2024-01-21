e4a03bdbd7b804be660f328fa4328c84cd1cc6ac
boms <beomsuyoo@gmail.com>
2024-01-21 14:07:48 +0900

    Title: 정수 사각형 최대 합, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240121/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
+++ 240121/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
@@ -23,7 +23,7 @@ int main() {
 
     for(int i=1;i<n;i++){
         for(int j=1;j<n;j++){
-            dp[i][j]=max(dp[i+1][j],dp[i][j-1])+map[i][j];
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1])+map[i][j];
         }
     }
     cout << dp[n-1][n-1];
