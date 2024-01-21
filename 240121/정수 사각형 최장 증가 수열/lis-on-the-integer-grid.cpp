8e184effb9309dabdf7355218e95a74659976234
boms <beomsuyoo@gmail.com>
2024-01-21 15:34:39 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1095ms, Memory: 2MB, Status: Time Limit Exceed - CodeTree

--- 240121/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 240121/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int n;
 int map[501][501];
-int dp[501][501];
 int visited[501][501];
+int dp[501][501];
 int dx[]={-1,1,0,0};
 int dy[]={0,0,-1,1};
 int mx=0;
@@ -18,17 +18,14 @@ void dynamic_programming(int x, int y){
 
         if(map[ny][nx]>map[y][x]&&!visited[ny][nx]){
             visited[ny][nx]=1;
-            dp[ny][nx]=dp[y][x]+1;
+            dp[ny][nx]=max(dp[ny][nx],dp[y][x]+1);
             mx=max(mx,dp[ny][nx]);
             dynamic_programming(ny,nx);
-            visited[ny][nx]=0;
         }
     }
 }
 
 void init(){
-    memset(visited,0,sizeof(visited));
-
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             dp[i][j]=1;
@@ -43,10 +40,10 @@ int main() {
             cin >> map[i][j];
         }
     }
+    init();
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            init();
-            visited[i][j]=1;
+            memset(visited,0,sizeof(visited));
             dynamic_programming(j,i);
         }
     }
