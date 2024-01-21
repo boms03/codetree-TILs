14018e7513c1700083cb527ce01f615e1bb66e82
boms <beomsuyoo@gmail.com>
2024-01-21 16:07:59 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 108ms, Memory: 5MB, Status: Passed - CodeTree

--- 240121/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 240121/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -4,59 +4,6 @@
 #include <tuple>
 using namespace std;
 
-// int n;
-// int map[501][501];
-// int visited[501][501];
-// int dp[501][501];
-// int dx[]={-1,1,0,0};
-// int dy[]={0,0,-1,1};
-// int mx=0;
-
-// void dynamic_programming(int x, int y){
-//     for(int i=0;i<4;i++){
-//         int nx = x + dx[i];
-//         int ny = y + dy[i];
-//         if(nx<0||nx>=n||ny<0||ny>=n) continue;
-
-//         if(map[ny][nx]>map[y][x]&&!visited[ny][nx]){
-//             visited[ny][nx]=1;
-//             dp[ny][nx]=max(dp[ny][nx],dp[y][x]+1);
-//             mx=max(mx,dp[ny][nx]);
-//             dynamic_programming(ny,nx);
-//         }
-//     }
-// }
-
-// void init(){
-//     for(int i=0;i<n;i++){
-//         for(int j=0;j<n;j++){
-//             dp[i][j]=1;
-//         }
-//     }
-// }
-
-// int main() {
-//     cin >> n;
-//     for(int i=0;i<n;i++){
-//         for(int j=0;j<n;j++){
-//             cin >> map[i][j];
-//         }
-//     }
-//     init();
-//     for(int i=0;i<n;i++){
-//         for(int j=0;j<n;j++){
-//             memset(visited,0,sizeof(visited));
-//             dynamic_programming(j,i);
-//         }
-//     }
-//     cout << mx;
-//     return 0;
-
-// } -> dp 격자에서 어디서부터 시작하지? 작은문제는 어디서 시작?
-
-
-// dp 갱신 순서를 작은 수로 설정하자!
-
 int n;
 int map[501][501];
 int dp[501][501];
@@ -81,6 +28,7 @@ int main() {
             v.push_back(make_tuple(map[i][j],j,i));
         }
     }
+    sort(v.begin(),v.end());
     init();
 
     for(int i=0;i<v.size();i++){
@@ -95,7 +43,6 @@ int main() {
             }
         }
     }
-    sort(v.begin(),v.end());
     int mx=0;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
