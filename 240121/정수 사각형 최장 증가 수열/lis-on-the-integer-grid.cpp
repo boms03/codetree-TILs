1ae66287b8909a7513f6cacca229c44a7426baec
boms <beomsuyoo@gmail.com>
2024-01-21 15:25:22 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 7ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240121/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int n;
+int map[501][501];
+int dp[501][501];
+int visited[501][501];
+int dx[]={-1,1,0,0};
+int dy[]={0,0,-1,1};
+int mx=0;
+
+void dynamic_programming(int x, int y){
+    for(int i=0;i<4;i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(nx<0||nx>=n||ny<0||ny>=n) continue;
+
+        if(map[ny][nx]>map[y][x]&&!visited[ny][nx]){
+            visited[ny][nx]=1;
+            dp[ny][nx]=dp[y][x]+1;
+            mx=max(mx,dp[ny][nx]);
+            dynamic_programming(ny,nx);
+            visited[ny][nx]=0;
+        }
+    }
+}
+
+void init(){
+    memset(visited,0,sizeof(visited));
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            dp[i][j]=1;
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin >> map[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            init();
+            visited[i][j]=1;
+            dynamic_programming(j,i);
+        }
+    }
+    cout << mx;
+    return 0;
+
+}
