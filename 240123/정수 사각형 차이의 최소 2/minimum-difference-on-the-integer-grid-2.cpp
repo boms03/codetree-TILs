c3d4abb415c358ea1810ea9778c4f51cad0293c5
boms <beomsuyoo@gmail.com>
2024-01-23 11:37:08 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240123/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
+++ 240123/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
@@ -6,26 +6,7 @@ int dp[101][101];
 int map[101][101];
 int n;
 
-int solve(int lower_bound){
-    for(int i=1;i<n;i++){
-        for(int j=1;j<n;j++){
-            if(map[i][j]<lower_bound){
-                map[i][j]=1e9;
-            }
-            dp[i][j] = max(min(dp[i-1][j],dp[i][j-1]),map[i][j]);
-        }
-    }
-    return dp[n-1][n-1];
-}
-
-int main() {
-    cin >> n;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cin >> map[i][j];
-        }
-    }
-
+void init(){
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             dp[i][j]=1e9;
@@ -41,11 +22,42 @@ int main() {
     for(int i=1; i<n;i++){
         dp[i][0] = max(dp[i-1][0],map[i][0]);
     }
+}
+
+int solve(int lower_bound){
+
+    init();
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(map[i][j]<lower_bound){
+                map[i][j]=1e9;
+            }
+        }
+    }
+
+    for(int i=1;i<n;i++){
+        for(int j=1;j<n;j++){
+            dp[i][j] = max(min(dp[i-1][j],dp[i][j-1]),map[i][j]);
+        }
+    }
+
+    return dp[n-1][n-1];
+}
+
+int main() {
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> map[i][j];
+        }
+    }
 
     int ans = 1e9;
 
-    for(int lower_bound=1; lower_bound<100; lower_bound++){
+    for(int lower_bound=1; lower_bound<=100; lower_bound++){
         int upper_bound = solve(lower_bound);
+        
         if(upper_bound==1e9) continue;
         
         ans = min(ans,upper_bound-lower_bound);
