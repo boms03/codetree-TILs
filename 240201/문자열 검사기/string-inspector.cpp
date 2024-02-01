ed8f4145dccb0aaba2dd0f496b94c206d2fe934c
boms <beomsuyoo@gmail.com>
2024-02-01 11:18:27 +0900

    Title: 문자열 검사기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240201/문자열 검사기/string-inspector.cpp
+++ 240201/문자열 검사기/string-inspector.cpp
@@ -8,7 +8,7 @@ int main() {
     string s;
     cin >> s;
 
-    for(int i=0; i<s.size(); i++){
+    for(int i=0; i<s.length(); i++){
         if(s[i]=='('){
             stk.push('(');
         } else if(s[i]==')'){
