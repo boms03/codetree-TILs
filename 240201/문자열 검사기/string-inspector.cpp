b59e08dc84676a70736a984eda71090468bc2168
boms <beomsuyoo@gmail.com>
2024-02-01 11:17:40 +0900

    Title: 문자열 검사기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240201/문자열 검사기/string-inspector.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+int main() {
+
+    stack<char>stk;
+    string s;
+    cin >> s;
+
+    for(int i=0; i<s.size(); i++){
+        if(s[i]=='('){
+            stk.push('(');
+        } else if(s[i]==')'){
+            if(stk.top()=='('){
+                stk.pop();
+            }
+        } else if(s[i]=='['){
+            stk.push('[');
+        } else if(s[i]==']'){ 
+            if(stk.top()=='['){
+                stk.pop();
+            }
+        }
+    }
+    if(stk.empty()) cout << 1;
+    else cout << 0;
+    return 0;
+}
