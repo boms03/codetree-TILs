b61adf09c19d73e23dae6aae6e2680433f846d28
boms <beomsuyoo@gmail.com>
2024-03-01 19:39:46 +0900

    Title: 3개의 선 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240301/3개의 선 2/three-lines-2.cpp
+++ 240301/3개의 선 2/three-lines-2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int n;
     vector<pair<int,int>>v;
-
+    cin >> n;
     for(int i=0;i<n;i++){
         int x, y;
         cin >> x >> y;
@@ -14,7 +14,6 @@ int main() {
     for(int i=0;i<=10;i++){
         for(int j=0;j<=10;j++){
             for(int k=0;k<=10;k++){ // x 3개, x 2개 y1개, x1개 y2개, y3개
-
                 int cnt = 0;
                 for(int e=0;e<v.size();e++){
                     if(v[e].first==i || v[e].first==j || v[e].first==k){
