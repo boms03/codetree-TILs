b2cc4f7aaabdd2395f6fb627e9bd26f2fec4c75a
boms <beomsuyoo@gmail.com>
2024-03-01 22:33:11 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240301/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.cpp
+++ 240301/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <algorithm>
 using namespace std;
 int main() {
     int n;
@@ -12,18 +13,20 @@ int main() {
     //모든 부분문자열 구하기
     for(int i=0;i<n;i++){
         string temp = ""+s[i];
+        str.insert(temp);
         for(int j=0;j<n;j++){
             temp+=s[j];
             str.insert(temp);
         }
     }
-    int ans = 0;
+    int ans = 100;
     for(auto& st: str){
         int cnt = 0;
-        for(int i=0;i<n-st.length()+1;i++){
-            if(st == s.substr(i,st.length())) cnt++;
+        int len = st.length();
+        for(int i=0;i<n-len+1;i++){
+            if(st == s.substr(i,len)) cnt++;
         }
-        if(cnt<2) ans++;
+        if(cnt<2) ans=min(ans,len);
     }
     cout << ans;
 }
