bdf540c2c1e591c9e38a2e33e2759754b2016c51
boms <beomsuyoo@gmail.com>
2024-03-01 19:25:50 +0900

    Title: 3개의 선 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240301/3개의 선 2/three-lines-2.cpp
+++ 240301/3개의 선 2/three-lines-2.cpp
@@ -4,29 +4,27 @@ using namespace std;
 int main() {
     int n;
     vector<pair<int,int>>v;
+
     for(int i=0;i<n;i++){
         int x, y;
         cin >> x >> y;
         v.push_back({x,y});
     }
+
     for(int i=0;i<=10;i++){
-        int cnt=0;
-        for(int j=0;j<n;j++){
-            if(i==v[j].first) cnt++;
-        }
-        if(cnt==n){
-            cout << 1;
-            exit(0);
-        }
-    }
-    for(int i=0;i<=10;i++){
-        int cnt=0;
-        for(int j=0;j<n;j++){
-            if(i==v[j].second) cnt++;
-        }
-        if(cnt==n){
-            cout << 1;
-            exit(0);
+        for(int j=0;j<=10;j++){
+            for(int k=0;k<=10;k++){
+                int cnt=0;
+                if(i==j || j==k || i==k) continue;
+                for(int j=0;j<n;j++){
+                    if(i==v[j].first || j==v[j].first || k==v[j].first) cnt++;
+                    if(i==v[j].second || j==v[j].second || k==v[j].second) cnt++;
+                }
+                if(cnt==n){
+                    cout << 1;
+                    exit(0);
+                }
+            }
         }
     }
     cout << 0;
