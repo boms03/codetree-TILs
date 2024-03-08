02459f211048f820527b0b15f102cc2ddc6096dd
boms <beomsuyoo@gmail.com>
2024-03-08 16:49:47 +0900

    Title: 바구니 안의 사탕 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240308/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 240308/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -16,11 +16,21 @@ int main() {
 
     int max_cnt = 0;
 
-    // 모든 시작점을 탐색하기: 구간은 [시작,시작+2*K]
-    for(int i=0;i<=MAX_N-2*k;i++){
+    // // 범위 자체가 벗어나도 [0,100] 사이에 있는 것을 구해야한다
+    // for(int i=0;i<=MAX_N-2*k;i++){
+    //     int cnt = 0;
+    //     for(int j=0;j<=2*k;j++){
+    //         cnt += arr[i+j];
+    //     }
+    //     max_cnt = max(max_cnt,cnt);
+    // }
+    // cout << max_cnt;
+
+    // 중심
+    for(int i=0;i<=MAX_N;i++){
         int cnt = 0;
-        for(int j=0;j<=2*k;j++){
-            cnt += arr[i+j];
+        for(int j=i-k;j<=i+k;j++){
+            if(j>=0 && j<=MAX_N) cnt += arr[j];
         }
         max_cnt = max(max_cnt,cnt);
     }
