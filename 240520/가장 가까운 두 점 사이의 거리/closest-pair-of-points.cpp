f18ce18f14a8c2c7b529cd631dd57a50e1fd38e5
boms <beomsuyoo@gmail.com>
2024-05-20 19:54:03 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240520/가장 가까운 두 점 사이의 거리/closest-pair-of-points.cpp
+++ 240520/가장 가까운 두 점 사이의 거리/closest-pair-of-points.cpp
@@ -2,19 +2,22 @@
 #include <climits>
 #define MAX_N 100
 using namespace std;
+
+int x[MAX_N], y[MAX_N];
+int n;
+
+int dist(int i,int j){
+    return (x[i]-x[j])*(x[i]-x[j])+(y[i]-y[j])*(y[i]-y[j]);
+}
 int main() {
-    int x[MAX_N], y[MAX_N];
-    int n;
     cin >> n;
     for(int i=0;i<n;i++){
         cin >> x[i] >> y[i];
     }
     int min_dist = 1000000;
     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(i==j) continue;
-            int dist = (x[i]-x[j])*(x[i]-x[j])+(y[i]-y[j])*(y[i]-y[j]);
-            min_dist = min(min_dist,dist);
+        for(int j=i+1;j<n;j++){
+            min_dist = min(min_dist,dist(i,j));
         }
     }
     cout<<min_dist;
