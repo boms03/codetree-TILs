d80baeb70bdd27bdce8c0a0cd1f0329b1f190ed8
boms <beomsuyoo@gmail.com>
2024-05-21 14:56:41 +0900

    Title: 개발자의 순위, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240521/개발자의 순위/developer's-rank.cpp
+++ 240521/개발자의 순위/developer's-rank.cpp
@@ -10,7 +10,9 @@ int main() {
     cin >> n >> k;
     for(int i=0; i<n; i++){
         for(int j=0; j<k; j++){
-            cin >> rnk[i][j];
+            int number;
+            cin >> number;
+            rnk[i][number]=j;
         }
     }
 
@@ -18,32 +20,14 @@ int main() {
     for(int i=1;i<=k;i++){
         for(int j=1;j<=k;j++){
             if(i==j) continue;
-            bool check1 = true;
+            bool flag = true;
             for(int l=0;l<n;l++){
-                bool found_i = false;
-                bool found_j = false;
-                bool check2 = false;
-                for(int m=0;m<k;m++){
-                    if(!found_i && !found_j && rnk[l][m] == i){
-                        found_i = true;
-                        //cout << i << " " << l << " " << m << endl;
-                    }
-                    if(found_i && !found_j && rnk[l][m] == j){
-                        found_j = true;
-                        //cout << j << " " << l << " " << m << endl;
-                    }
-                    if(found_i && found_j){
-                        //cout << l << " " << m << " " << i << " " << j << endl;
-                        check2 = true;
-                        break;
-                    }
-                }
-                if(!check2){
-                    check1 = false;
+                if(rnk[l][i]>=rnk[l][j]){
+                    flag = false;
                     break;
                 }
             }
-            if(check1) count++;
+            if(flag) count++;
         }
     }
     cout << count;
