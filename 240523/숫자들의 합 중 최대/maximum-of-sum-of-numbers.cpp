25e348387449d1f1d1db6de28b29b8c505cb3933
boms <beomsuyoo@gmail.com>
2024-05-23 14:42:27 +0900

    Title: 숫자들의 합 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240523/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
+++ 240523/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
@@ -6,12 +6,12 @@ int main() {
     int max_sum = 0;
     for(int i=x;i<=y;i++){
         int sum = 0;
-        int temp = i;
-        sum += temp % 10;
-        int divide = 10;
-        while(temp/divide != 0){
-            sum += temp/divide;
-            divide *= 10;
+        int divisor = 1;
+        while(i/divisor != 0){
+            int quotient = i/divisor;
+            int place_value = quotient % 10;
+            sum += place_value;
+            divisor *= 10;
         }
         max_sum = max(max_sum,sum);
     }
