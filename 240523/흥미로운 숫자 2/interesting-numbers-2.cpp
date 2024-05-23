bb6c06a6fa6dc509bb9ad487cd64e5adea8164ea
boms <beomsuyoo@gmail.com>
2024-05-23 15:34:15 +0900

    Title: 흥미로운 숫자 2, Time: 20ms, Memory: 0MB, Status: Passed - CodeTree

--- 240523/흥미로운 숫자 2/interesting-numbers-2.cpp
+++ 240523/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -4,24 +4,25 @@ using namespace std;
 int x,y;
 int main() {
     cin >> x >> y;
-    int count = 0;
+    int answer = 0;
     for(int i=x; i<=y; i++){
         int digits[10] = {};
         int divisor = 1;
-        while(i/divisor!=0){
-            int quotient = i/divisor;
+        int length = 0;
+        while(i/divisor != 0){
+            int quotient = i / divisor;
             int place_value = quotient % 10;
             digits[place_value]++;
             divisor *= 10;
+            length++;
         }
-        bool flag = true;
-        int count_digit = 0;
+
+        bool flag = false;
         for(int j=0; j<10; j++){
-            if(digits[j]>0) count_digit++;
-            if(count_digit>2) break;
+            if(digits[j]==length-1) flag = true;
         }
-        if(count_digit == 2) count++;
+        if(flag) answer++;
     }
-    cout << count;
+    cout << answer;
     return 0;
 }
