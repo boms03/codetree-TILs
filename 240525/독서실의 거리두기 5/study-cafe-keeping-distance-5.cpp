14babf16a0ac73aa36442ca9f48d4a63a48e0093
boms <beomsuyoo@gmail.com>
2024-05-25 15:48:27 +0900

    Title: 독서실의 거리두기 5, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240525/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 240525/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#define MAX_N 20
 using namespace std;
 
 string seat;
@@ -12,16 +13,18 @@ int main() {
     for(int i=0;i<n;i++){
         if(seat[i]=='1') continue;
         seat[i] = '1';
-        bool flag = false;
+        int count = MAX_N;
         for(int j=0;j<n;j++){
+            bool flag = false;
             for(int k=j+1;k<n;k++){
                 if(seat[j] == '1' && seat[k] == '1'){
-                    max_count = max(max_count,k-j-1);
+                    count = min(count,k-j);
                     flag = true;
                 }
                 if(flag) break;
             }
         }
+        max_count = max(max_count,count);
         seat[i] = '0';
     }
     cout << max_count;
