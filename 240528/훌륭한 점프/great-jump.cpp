8285e9fce21cf27511ee3ff51d7c83a6ab764c8e
boms <beomsuyoo@gmail.com>
2024-05-28 00:15:56 +0900

    Title: 훌륭한 점프, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240528/훌륭한 점프/great-jump.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#define MAX_N 101
+using namespace std;
+
+int n,k;
+int num[MAX_N];
+int main() {
+    cin >> n >> k;
+    for(int i=0;i<n;i++){
+        cin >> num[i];
+    }
+    for(int i=max(num[0],num[n-1]);i<=n;i++){
+        int last_idx = 0;
+        bool flag = true;
+        for(int j=0;j<n;j++){
+            if(num[j]<=i){
+                if(j-last_idx>k){
+                    flag = false;
+                    break;
+                }
+                last_idx = j;
+            } 
+        }
+        if(flag){
+            cout << i;
+            break;
+        }
+    }
+
+    return 0;
+}
